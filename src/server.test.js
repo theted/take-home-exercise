32872dc22c12a8f56eb43b564e6008d46367eb65
Fredrik Sundberg <ted.sundberg@gmail.com>
2024-03-27 14:59:30 +0100

    feat: Add tests

--- src/server.test.js
+++ src/server.test.js
@@ -1,17 +1,78 @@
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "./typeDefs";
 import { resolvers } from "./resolvers";
-import { it, test, expect, assert } from "vitest";
+import { it, expect } from "vitest";
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-it("returns ticket with specification", async () => {
+it("returns tickets without parents", async () => {
   const response = await server.executeOperation({
-    query: "query Tickets() { title }"
+    query: "query { tickets { title }}"
   });
 
-  console.log(response);
+  expect(response.data.tickets[0].title).toBe("Foo");
+});
+
+it("returns a single ticket", async () => {
+  const response = await server.executeOperation({
+    query: "query($id: ID!) {  ticket(id: $id) { title } }",
+    variables: { id: 1 }
+  });
+
+  expect(response.data.ticket.title).toBe("Foo");
+});
+
+it("creates a ticket", async () => {
+  const response = await server.executeOperation({
+    query:
+      'mutation { createTicket(title: "Bar", isCompleted: false) { title } }'
+  });
+
+  expect(response.data.createTicket.title).toBe("Bar");
+});
+
+it("updates ticket", async () => {
+  const response = await server.executeOperation({
+    query: 'mutation { updateTicket(id: 3, title: "Baz") { title } }'
+  });
+
+  expect(response.data.updateTicket.title).toBe("Baz");
+});
+
+it("sets parent of ticket", async () => {
+  const response = await server.executeOperation({
+    query: "mutation { setParentOfTicket(parentId: 1, childId: 2) { title } }"
+  });
+
+  expect(response.data.setParentOfTicket.title).toBe("Bar");
+});
+
+it("adds children to ticket", async () => {
+  const response = await server.executeOperation({
+    query:
+      "mutation { addChildrenToTicket(parentId: 1, childrenIds: [2, 3]) { title children { id title } } }"
+  });
+
+  expect(response.data.addChildrenToTicket.children[0].id).toBe("2");
+});
+
+it("removes parent from ticket", async () => {
+  const response = await server.executeOperation({
+    query:
+      "mutation { removeParentFromTicket(id: 1) { title children { id title } } }"
+  });
+
+  console.log(response.data.removeParentFromTicket);
+  expect(response.data.removeParentFromTicket.title).toBe("Foo");
+});
+
+it("toggles ticket", async () => {
+  const response = await server.executeOperation({
+    query: "mutation { toggleTicket(id: 1, isCompleted: true) { isCompleted } }"
+  });
+
+  expect(response.data.toggleTicket.isCompleted).toBe(true);
 });
